0df26589888f6ac44d63dbf242f1e6a75a0145cf
Arnaldo Willian <arnaldowillian2@gmail.com>
2023-12-05 21:44:27 -0300

    Projeto AV2 Professor Diogo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import RoutesApp from "./routes";
+import { AuthProvider } from "./contexts/auth";
+import GlobalStyle from "./styles/global";
+import sapatoImage from "./components/Imagem/sapatos.jpeg"; 
 
-function App() {
+const App = () => {
+  const headerStyle = {
+    padding: "90px",
+    backgroundColor: "#333",
+    color: "#fff",
+    textAlign: "center",
+    backgroundImage: `url(${sapatoImage})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
+
+  const footerStyle = {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    padding: "20px",
+    backgroundColor: "#333",
+    color: "#fff",
+    textAlign: "center",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <div style={{ minHeight: "100vh", position: "relative" }}>
+        <header style={headerStyle}>
+          <h1>OLShoes, a sua loja de tênis favorita.</h1>
+        </header>
+        <RoutesApp />
+        <footer style={footerStyle}>
+          <p>Bem-Vindo a maior loja de tênis do Brasil!</p>
+        </footer>
+      </div>
+      <GlobalStyle />
+    </AuthProvider>
   );
-}
+};
 
 export default App;
