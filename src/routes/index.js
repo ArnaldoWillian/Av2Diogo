09c4dd0f7f010bf6cee5a158b4cb9b32cc8703c0
Arnaldo Willian <arnaldowillian2@gmail.com>
2023-12-05 21:47:03 -0300

    Projeto AV2 final

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import { Fragment } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
-import Home from "../pages/Home";
-import Signin from "../pages/Signin";
+import Home from "../pages/Inicial";
+import Signin from "../pages/login";
 import Signup from "../pages/Signup";
 
 const Private = ({ Item }) => {
